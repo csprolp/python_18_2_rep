eb1f94edde9eea621f41ab7ced6d032edb655f10
Vyacheslav Yushmanov <csprolp@gmail.com>
2025-03-12 21:47:00 +0300

    внес правки после ревью

--- test_homework_lesson_6.py
+++ test_homework_lesson_6.py
@@ -28,14 +28,10 @@ def test_dark_theme_by_time_and_user_choice():
     is_dark_theme = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user is None:
-        if 6 <= current_time.hour >= 22:
-            is_dark_theme = True
-        else: is_dark_theme = False
-    elif dark_theme_enabled_by_user is True:
-        is_dark_theme = True
-    else: is_dark_theme = False
-
+    if dark_theme_enabled_by_user is not None:
+        is_dark_theme = dark_theme_enabled_by_user
+    else:
+        is_dark_theme = (6 <= current_time.hour < 22)
 
     assert is_dark_theme is True
 
